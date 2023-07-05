6d5925c1de55bcd162e1ba3ca8f556a248f5c842
Owen Kosman <owenkosman@Owens-MacBook-Pro-3.local>
2023-07-05 07:50:55 +0100

    changed routing again

--- application/middleware.ts
+++ application/middleware.ts
@@ -14,9 +14,12 @@ export async function middleware(req: NextRequest) {
   } = await supabase.auth.getUser()
 
   // if user is signed in and the current path is / redirect the user to /dashboard
-  if (user && (req.nextUrl.pathname === '/' || req.nextUrl.pathname === '/dashboard' )) {
-    return NextResponse.redirect(new URL('/dashboard/home', req.url))
+  if (user) {
+    if (req.nextUrl.pathname === '/' || req.nextUrl.pathname === '/dashboard' ) {
+      return NextResponse.redirect(new URL('/dashboard/home', req.url))
+    }
   }
+    
 
   // if user is not signed in and the current path is not / redirect the user to /login
   if (!user) {
