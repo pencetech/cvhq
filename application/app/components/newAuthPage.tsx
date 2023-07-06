9d476a51b16021664422ace876567a6525352a6b
Owen Kosman <owenkosman@Owens-MacBook-Pro-3.local>
2023-07-06 14:57:33 +0100

    updated middleware

--- application/app/components/newAuthPage.tsx
+++ application/app/components/newAuthPage.tsx
@@ -40,7 +40,7 @@ export default function Login() {
     let { error } = await supabase.auth.verifyOtp(verifyOpts);
     if (error) messageApi.error("error authenticating.")
     setLoading(false);
-    router.push("dashboard/home")
+    router.push("/dashboard/home")
   }
 
   return (
