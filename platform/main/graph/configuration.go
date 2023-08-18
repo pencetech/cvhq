5530ab4f7e2dd19d9982a43e33841053b0529e7b
Owen Kosman <owenkosman@Owens-MBP-3.cust.communityfibre.co.uk>
2023-08-18 21:16:45 +0100

    configuration update

--- platform/main/graph/configuration.go
+++ platform/main/graph/configuration.go
@@ -3,14 +3,20 @@ package graph
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/BurntSushi/toml"
+	"github.com/joho/godotenv"
 )
 
 type Configuration struct {
 	DefaultPort string
 	EnhancementPrompt int
 	SummaryPrompt int
+	PDFUrl string
+	AWSAccessKey string
+	AWSSecretKey string
+	OpenAIKey string
 }
 
 func NewConfiguration() *Configuration {
@@ -19,5 +25,19 @@ func NewConfiguration() *Configuration {
 		log.Println("Error decoding configuration -> ", err)
 	}
 	fmt.Println("Configuration added")
+
+	if os.Getenv("APP_ENV") != "prod" ||  
+	os.Getenv("APP_ENV") != "demo" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env")
+		}
+	}
+
+	conf.AWSAccessKey = os.Getenv("AWS_ACCESS_KEY")
+	conf.AWSSecretKey = os.Getenv("AWS_SECRET_KEY")
+	conf.OpenAIKey = os.Getenv("OPENAI_KEY")
+	conf.PDFUrl = os.Getenv("PDF_URL")
+	
 	return &conf
 }
