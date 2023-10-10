a2b5a86ec5d5e9ac0cb42126217d82d3ce98e068
Owen Kosman <owenkosman@Owens-MBP-3.cust.communityfibre.co.uk>
2023-10-10 10:23:05 +0100

    fixed ui

--- application/app/components/chat.tsx
+++ application/app/components/chat.tsx
@@ -10,12 +10,7 @@ const Chat = ({ name, chatHistory }: { name: string, chatHistory: ChatHistory[] 
 
     const ChatBubble = (type: "start" | "end", content: string) => (
         <div className={`chat chat-${type} ${type === "end" ? "place-items-end grid-cols-[1fr_auto] grid-rows-none" : ""}`}>
-            <div className="avatar placeholder">
-                <div className="bg-neutral-focus text-neutral-content rounded-full w-10">
-                    <span>{type === "start" ? "A" : name[0]}</span>
-                </div>
-            </div>
-            <div className={`chat-bubble chat-bubble-${type === "start" ? "primary" : "info"}`}>{content}</div>
+            <div className={`chat-bubble chat-bubble-${type === "start" ? "primary" : "info"}`} style={{ gridColumnStart: "1" }}>{content}</div>
         </div>
     )
 
