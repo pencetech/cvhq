8c4ac7ba22afb8c38c01c81db596b55f51251642
Owen Kosman <owenkosman@Owens-MBP-3.cust.communityfibre.co.uk>
2023-10-10 10:43:34 +0100

    added darker color:

--- application/app/components/chat.tsx
+++ application/app/components/chat.tsx
@@ -9,7 +9,7 @@ const Chat = ({ name, chatHistory }: { name: string, chatHistory: ChatHistory[] 
 
     const ChatBubble = (type: "start" | "end", content: string) => (
         <div className={`chat chat-${type} ${type === "end" ? "place-items-end grid-cols-[1fr_auto] grid-rows-none" : ""}`}>
-            <div className={`chat-bubble`} style={{ gridColumnStart: "1", backgroundColor: (type === "start" ? "#bae637" : "#ffec3d") }}>{content}</div>
+            <div className={`chat-bubble`} style={{ gridColumnStart: "1", backgroundColor: (type === "start" ? "#3f6600" : "#876800") }}>{content}</div>
         </div>
     )
 
